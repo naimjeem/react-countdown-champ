96d9b830768d40c0c62732de84238e71fe51d7d5
Naim Jeem <naim36208@gmail.com>
2018-01-13 01:08:50 +0600

    Changing state with input value

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'January 18, 2018',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({ deadline: this.state.newDeadline })
+  }
+
   render() {
     return (
       <div className="app">
-        <div className="app-title">Countdown to January 20, 2018</div>
+        <div className="app-title">Countdown to {this.state.deadline}</div>
         <div>
           <div className="clock-days">10 days</div>
           <div className="clock-hrs">30 hours</div>
           <div className="clock-mins">40 minutes</div>
           <div className="clock-sec">20 seconds</div>
         </div>
-        <input placeholder="New date" type="text" name="" id=""/>
-        <button>Submit</button>
+        <input placeholder="New date" onChange={ event => {this.setState({ newDeadline: event.target.value })} } />
+        <button onClick={ () => this.changeDeadline() }>Submit</button>
       </div>
     )
   }
