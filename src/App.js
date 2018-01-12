57bb381be51fd5462708ea9a152dbfffad8dccb5
Naim Jeem <naim36208@gmail.com>
2018-01-13 02:51:11 +0600

    Countdown timer added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// Component
+import Clock from './Clock';
 // Stylesheet
 import './App.css';
 
@@ -19,13 +21,8 @@ class App extends Component {
     return (
       <div className="app">
         <div className="app-title">Countdown to {this.state.deadline}</div>
-        <div>
-          <div className="clock-days">10 days</div>
-          <div className="clock-hrs">30 hours</div>
-          <div className="clock-mins">40 minutes</div>
-          <div className="clock-sec">20 seconds</div>
-        </div>
-        <input placeholder="New date" onChange={ event => {this.setState({ newDeadline: event.target.value })} } />
+        <Clock deadline={this.state.deadline} />
+        <input placeholder="January 18, 2019" onChange={ event => {this.setState({ newDeadline: event.target.value })} } />
         <button onClick={ () => this.changeDeadline() }>Submit</button>
       </div>
     )
---
+++ src/Clock.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+// Stylesheet
+import './App.css';
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  }
+
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+  }
+
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor((time / 1000) % 60);
+    const minutes = Math.floor((time / 1000 / 60) % 60);
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    const days = Math.floor((time / (1000 * 60 * 60 * 24)));
+
+    this.setState({days, hours, minutes, seconds});
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="clock-days">{this.state.days} days</div>
+        <div className="clock-hrs">{this.state.hours} hours</div>
+        <div className="clock-mins">{this.state.minutes} minutes</div>
+        <div className="clock-sec">{this.state.seconds} seconds</div>
+      </div>
+    )
+  }
+}
+
+export default Clock;
